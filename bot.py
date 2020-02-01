1ef8893553c3a5e0f7ef95a8829a7839145dd226
Вовий Зябиков <zyablik.akay.noob@mail.ru>
2020-02-01 23:37:29 +0300

    ee

--- bot.py
+++ bot.py
@@ -4,6 +4,11 @@ bot = telebot.TeleBot("1016441239:AAF42LkLUVRBbvvyFG5XgiBD567R-y2rfik")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, "Здарова карова, как тибя звати?")
+
+@bot.message_handler(content_types=['text'])
+def shitherewegoagain(message):
+    bot.send_message(message, "Не мешася уди")
 
 bot.polling()
+
