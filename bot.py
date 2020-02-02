370149eb3ce8f141b4c9abf05cb3c7ef6aca3e45
Вовий Зябиков <zyablik.akay.noob@mail.ru>
2020-02-02 16:08:12 +0300

    11112

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
 import telebot
+import random
 
 bot = telebot.TeleBot("1016441239:AAF42LkLUVRBbvvyFG5XgiBD567R-y2rfik")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, "Привет")
+
+@bot.message_handler(commands=['random'])
+def randome(message):
+    bot.reply_to(message, str(random.randint(1,100)))
 
 bot.polling(none_stop=True)
 
